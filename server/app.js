c1c1255ad157e0b86dcf0fe48761c410a219b68e
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-05-22 08:50:53 +0200

    docs

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const FileStore = require('session-file-store')(session)
 // This module is necessary to get the data sent from a client (from a FROM or via AJAX).
 const bodyParser = require('body-parser')
 
-// We instantiate an `express()` application into the `app` variable.
+// We instantiate an `express()` application into the `app` variable.<br/>
 // So `app` will inherite methods such as `app.use`, `app.get`, `app.set`...
 const app = express()
 // We set the port as a constant.
@@ -19,9 +19,9 @@ const PORT = 3000
 // We load a router defined in another file that contain the logic for all the `users` routes
 const userRoutes = require('./routes/users')
 
-// We set this app (through _middlewares_) to use sessions w/ this configuration.
-// Every user requesting some endpoint from this server will be assigned to a session _space_ in the server that will be unique for every user (controller by cookies on the client).
-// The data of sessions will be available on every request in `req.session`.
+// We set this app (through _middlewares_) to use sessions w/ this configuration.<br/>
+// Every user requesting some endpoint from this server will be assigned to his own _session_ in the server that will be unique for every user (controller by cookies on the client).<br/>
+// The data of the user's sessions will be available on every request in `req.session`.
 app.use(session({
   name: 'jm-server-session-cookie-id',
   secret: '4u6mVaJtJrrhZb2iHx2ugBof',
@@ -30,8 +30,8 @@ app.use(session({
   store: new FileStore()
 }))
 
-// We set this app (through _middlewares_) to parse the data received from the client.
-// This data will be available in `req.body`.<br/>
+// We set this app (through _middlewares_) to parse the data received from the client.<br/>
+// This data will be available in `req.body`.
 
 // [1] To properly parse data received from a FORM.
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -40,20 +40,22 @@ app.use(bodyParser.json())
 
 // We set this app (through _middlewares_) to initialize the _variable_ session `users` as an empty array the first time the session in created
 app.use((req, res, next) => {
-  req.session.users = req.session.users || []
+  req.session.users = req.session.users || []
   next()
 })
 
 // We set this app (through _middlewares_) to use the `../client` as our _public_ folder (the one where express will look for css, js, images, html, etc...)
-app.use(express.static( path.join(__dirname, '../client')  ))
+app.use(express.static(path.join(__dirname, '../client')))
 
 // We set our app to use `pug` as our template engine and the folder where to look for these templates
 app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, '/views'))
 
 // We set this to _prettify_ the generated HTML
-app.locals.pretty = true;
+app.locals.pretty = true
 
+// We link (couple, add) the set of routes (and their logic) defined in another `router`
 app.use(userRoutes)
 
+// Start listining to requests
 app.listen(PORT, () => console.log(`🤘 Magic happens at PORT ${PORT}...`))
